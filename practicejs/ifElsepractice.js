d32d5c1bdad20d2f12f5a819caef6c13becb34e0
Carlos Velez <tradingwithcarlos@gmail.com>
2020-11-19 16:32:30 -0500

    If else practice
    Practice the IF Else statements

---
+++ practicejs/ifElsepractice.js
@@ -0,0 +1,29 @@
+//First Exercise
+var x = 9;
+
+if(x>0){
+    console.log("Positive");
+} else{
+    console.log("Negative");
+}
+
+
+// Age restriction exercise
+var age = 20;
+
+if (age >= 18) {
+    console.log("Access Granted");
+} else{
+    console.log("Access Denied");
+}
+
+// Greater than or less than 100 exercise
+var num = 0;
+
+if (num > 100) {
+    console.log("Positive and Greater than 100");
+} else if (num >= 0 && num < 100){
+    console.log("Positive and Less than 100");
+} else if (num < 0){
+    console.log("Negative");
+}
