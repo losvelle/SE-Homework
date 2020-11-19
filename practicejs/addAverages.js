4aea5903c03c824bcb2c2249b20aa05f380b7ce7
Carlos Velez <tradingwithcarlos@gmail.com>
2020-11-19 10:10:21 -0500

    Javascript Practice
    Practicing javascript and using command prompt to print answer

---
+++ practicejs/addAverages.js
@@ -0,0 +1,8 @@
+//Find the average of ( 4, 10, 22, -30, 55); Print in console
+//Find the average of ( 68, 7, -22, 9, 100); Print in console
+
+var avg1 = ( 4 + 10 + 22 + (-30) + 55) / 5;
+
+
+var avg2 = ( 68+ 7 + (-22) + 9 + 100) / 5; 
+console.log(avg1, avg2)
---
+++ practicejs/addingTwoNumbers.js
@@ -0,0 +1,25 @@
+// var num1, num2;
+//num1 = 1;
+//num2 = 2;
+
+//var sum = num1 + num2;
+//
+
+var n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+n1 = 1;
+n2 = 2;
+n3 = 3;
+n4 = 4;
+n5 = 5;
+n6 = 6;
+n7 = 7;
+n8 = 8;
+n9 = 9;
+n10 = 10;
+
+var avg1 = (n1 + n2 + n3 + n4 + n5) / 5
+var avg2 = (n6 + n7 + n8 + n9 + n10) / 5
+
+var avg3 = (avg1 + avg2)/ 5
+
+console.log("First Average",avg1, "Second Average", avg2, "Total Average",avg3);
