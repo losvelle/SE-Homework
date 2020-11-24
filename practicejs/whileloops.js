96edf99b3a00c5b3c57f7f73dfde27e1bc255828
Carlos Velez <tradingwithcarlos@gmail.com>
2020-11-24 14:19:20 -0500

    my first for each loop

---
+++ practicejs/whileloops.js
@@ -0,0 +1,18 @@
+// do while
+var x = 1;
+do {
+    console.log(x);
+    x++;
+}while (x <= 10);
+
+//while loop
+var x = 1;
+while(x<=10){
+    console.log(x)
+    x++;
+}
+
+//for each loops
+var sub = [1, 2, 3];
+for( i in sub)
+    console.log(sub[i]);
