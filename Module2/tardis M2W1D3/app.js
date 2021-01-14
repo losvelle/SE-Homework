c5f50a3761121951f0da112a1253a96170e3221a
Carlos Velez <tradingwithcarlos@gmail.com>
2021-01-14 08:57:37 -0500

    React Dr.Who Tardis

---
+++ Module2/tardis M2W1D3/app.js
@@ -0,0 +1,69 @@
+class App extends React.Component {
+    state = {
+        tardis: {
+            name: 'Time and Relative Dimension in Space',
+            caps: false
+        }
+    }
+
+    changeIt = (text) => {
+        if (this.state.tardis.caps) {
+            this.setState({
+                tardis: {
+                    name: text.toLowerCase(),
+                    caps: false
+                }
+            })
+        } else {
+            this.setState({
+                tardis: {
+                    name: text.toUpperCase(),
+                    caps: true
+                }
+            })
+        }
+    }
+
+    render() {
+        //        VEventListenerV      VfunctionV        V parameter V
+        return (<div onClick= { () => this.changeIt(this.state.tardis.name)}>
+            <DivOne datta={this.state.tardis.name}/>
+
+        </div>)
+    }
+}
+
+class DivOne extends React.Component {
+    render() {
+        return (<div>
+            <DivTwo datta={this.props.datta}/>
+        </div>)
+    }
+}
+
+class DivTwo extends React.Component {
+    render() {
+        return (<div>
+            <DivThree datta={this.props.datta}/>
+        </div>)
+    }
+}
+
+class DivThree extends React.Component {
+    render() {
+        return (<div>
+            <DivFour datta={this.props.datta}/>
+        </div>)
+    }
+}
+
+class DivFour extends React.Component {
+    render() {
+        return (<div>
+            <h3>{this.props.datta}</h3>
+        </div>)
+    }
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('#container'));
